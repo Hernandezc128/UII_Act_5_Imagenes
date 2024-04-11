7c95b6bd26cf0244bdf003f6cc0b06b9bbb35230
CARLOS_IVAN_HERNANDEZ_MONROY <a.21308051280371@cbtis128.edu.mx>
2024-04-11 17:26:01 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,55 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Mostrar Imagenes Ivan Hernandez '),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 250.0,
+            width: 450.0,
+            child: Image.asset(
+              "assets/gato2.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 250.0,
+            width: 450.0,
+            child: Image.network(
+              "https://static.fundacion-affinity.org/cdn/farfuture/PVbbIC-0M9y4fPbbCsdvAD8bcjjtbFc0NSP3lRwlWcE/mtime:1643275542/sites/default/files/los-10-sonidos-principales-del-perro.jpg",
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      )),
     );
   }
 }
